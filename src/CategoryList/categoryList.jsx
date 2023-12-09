eccc40e994cd502e2d852841c66fbddb9a099484
Rasoulmadani <ali.rahbary@chmail.ir>
2023-12-09 11:58:25 +0330

    پیاده سازی کامپوننت لودینگ و استفاده در هنگام گرفتن دسته بندی های کلی سایت

--- src/CategoryList/categoryList.jsx
+++ src/CategoryList/categoryList.jsx
@@ -1,37 +1,48 @@
 import { useEffect } from "react";
 import axios from "../axios";
 import { useState } from "react";
+import Loading from "../Loading/loagind";
 
 const CategoryList = () => {
+  const [loading, setLoading] = useState(true);
   const [categories, setCategories] = useState([]);
   useEffect(() => {
     const fetchCategories = async () => {
       const response = await axios.get("/FoodCategory/categories");
       setCategories(response.data);
+      setLoading(false);
     };
     fetchCategories();
   }, []);
+  const renderContent = ()=>{
+    if(loading){
+      return <Loading theme="primary"/>
+    }else{
+      return (
+        <ul className="nav">
+        <li className="nav-item">
+          <a href="#" className="nav-link">
+            همه فست فودها
+          </a>
+        </li>
+        {categories.map((category) => (
+          <li className="nav-item" key={category.id}>
+            <a href="#" className="nav-link">
+              {category.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+      )
+    }
+  }
   return (
     <nav className="container mt-n5">
       <div
         className="d-flex align-items-center bg-white rounded-3 shadow-lg py-4"
         style={{ height: "80px" }}
       >
-        <ul className="nav">
-          <li className="nav-item">
-            <a href="#" className="nav-link">
-              همه فست فودها
-            </a>
-          </li>
-          {
-            categories.map(category=>(
-              <li className="nav-item" key={category.id}>
-                <a href="#" className="nav-link">{category.name}</a>
-              </li>
-            ))
-          }
-        </ul>
-        
+        {renderContent()}
       </div>
     </nav>
   );
