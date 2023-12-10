694c767e60625624dff6458d0352af0538f2d692
Rasoulmadani <ali.rahbary@chmail.ir>
2023-12-10 12:54:29 +0330

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/tutorial",
   plugins: [react()],
   // css:{
   //   modules:{
@@ -11,4 +12,4 @@ export default defineConfig({
   //   }
 
   // }
-})
+});
