a890275ba6a4491eeafd2b27de6d4c987773ba0e
Rasoulmadani <ali.rahbary@chmail.ir>
2023-12-10 13:17:58 +0330

    sdf

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/tutorial",
   plugins: [react()],
-  // css:{
-  //   modules:{
-  //     localsConvention:"camelCase",
-  //     generateScopedName:"[local]_[hash:base64:2]"
-  //   }
-
-  // }
 });
