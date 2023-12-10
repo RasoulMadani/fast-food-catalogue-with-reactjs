dee9802bdd3d92ffc9f588311efaa80baa9d0104
Rasoulmadani <ali.rahbary@chmail.ir>
2023-12-10 10:16:29 +0330

    دادن حالت انیمیشن به المنت ها موقعی که وارد صفحه می شوند

--- src/FastFoodList/fastFoodList.jsx
+++ src/FastFoodList/fastFoodList.jsx
@@ -1,15 +1,19 @@
 import FastFoodItem from "../FastFoodItem/fastFoodItem";
 
-const FastFoodList = ({fastFoodItems}) => {
-    return ( <div className="row">
-        {fastFoodItems.map(fastFood=>{
-            return (
-                <div className="col-md-4 col-sm-6 mb-grid-gutter" key={fastFood.id}>
-                    <FastFoodItem {...fastFood}/>
-                </div>
-            )
-        })}
-    </div> );
-}
- 
+const FastFoodList = ({ fastFoodItems }) => {
+  let delay = 0.1;
+  return (
+    <div className="row">
+      {fastFoodItems.map((fastFood) => {
+        delay += 0.03;
+        return (
+          <div className="col-md-4 col-sm-6 mb-grid-gutter" key={fastFood.id}>
+            <FastFoodItem {...fastFood} delay={delay} />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 export default FastFoodList;
